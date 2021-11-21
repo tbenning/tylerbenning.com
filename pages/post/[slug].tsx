9a3978ca9d6074a813da3861ce2e0044981e6b17
Tyler Benning <tbenning27@gmail.com>
2021-11-20 23:18:00 -0800

    added field-document support to post

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,71 @@
+import {
+    GetStaticPathsResult,
+    GetStaticPropsContext,
+    InferGetStaticPropsType,
+  } from 'next';
+  import Link from 'next/link';
+  import { DocumentRenderer, DocumentRendererProps } from '@keystone-next/document-renderer';
+  import { query } from '.keystone/api';
+  
+  const renderers: DocumentRendererProps['renderers'] = {
+    // use your editor's autocomplete to see what other renderers you can override
+    inline: {
+      bold: ({ children }) => {
+        return <strong>{children}</strong>;
+      },
+    },
+
+    block: {
+      paragraph: ({ children, textAlign }) => {
+        return <p style={{ textAlign }}>{children}</p>;
+      },
+    },
+  };
+
+  export default function PostPage({
+    post,
+  }: InferGetStaticPropsType<typeof getStaticProps>) {
+    return (
+      <div>
+        <main style={{margin: "3rem"}}>
+        <div>
+          <Link href="/">
+            <a>&larr; back home</a>
+          </Link>
+        </div>
+        <h1>{post.title}</h1>
+
+        {/* <p>{post.content}</p> */}
+        {post.content?.document && (
+        <DocumentRenderer document={post.content.document} renderers={renderers} />
+      )}
+        </main>
+      </div>
+    );
+  }
+  
+  export async function getStaticPaths(): Promise<GetStaticPathsResult> {
+    const posts = await query.Post.findMany({
+      query: `slug`,
+    });
+  
+    const paths = posts
+      .map(post => post.slug)
+      .filter((slug): slug is string => !!slug)
+      .map(slug => `/post/${slug}`);
+  
+    return {
+      paths,
+      fallback: false,
+    };
+  }
+  
+  export async function getStaticProps({
+    params,
+  }: GetStaticPropsContext) {
+    const post = await query.Post.findOne({
+      where: { slug: params!.slug as string },
+      query: 'id title content {document}',
+    });
+    return { props: { post } };
+  }
