2fdf15489b290691e2dad93b1d026582b35eec66
Tyler Benning <tbenning27@gmail.com>
2021-12-05 20:40:51 -0800

    add hover to chips

--- components/Capsule/index.tsx
+++ components/Capsule/index.tsx
@@ -1,7 +1,7 @@
 export default function Capsule({ text, className }) {
   return (
     <div
-      className={`inline-block border border-solid border-gray-300 rounded-full py-2 px-3 ${className}`}
+      className={`inline-block border border-solid border-gray-300 rounded-full py-2 px-3 transition duration-300 ease-in-out hover:bg-gray-200 hover:cursor-default hover:border-transparent ${className}`}
     >
       <span className="text-sm text-secondary">{text}</span>
     </div>
--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -3,7 +3,7 @@ import Nav from "../Nav"
 export default function Layout({ children }) {
   return (
     <>
-      <div className="container px-4 mx-auto max-w-screen-xl">
+      <div className="container px-4 mx-auto md:px-10 max-w-screen-xl">
         <Nav />
         <div className="pt-4">{children}</div>
       </div>
