bb7a68808c7d240ce7e1fb130cf197c681f0bf6f
Tyler Benning <tbenning27@gmail.com>
2021-12-07 23:09:20 -0800

    first typescript typing

--- components/Capsule/index.tsx
+++ components/Capsule/index.tsx
@@ -1,4 +1,10 @@
-export default function Capsule({ text, className, icon }) {
+type Props = {
+  text: string
+  className: string
+  icon: JSX.Element
+}
+
+export default function Capsule({ text, className, icon }: Props) {
   return (
     <div
       className={`inline-block border flex items-center border-solid border-gray-300 rounded-full py-2 px-3 transition duration-300 ease-in-out hover:bg-gray-200 hover:cursor-default hover:border-transparent ${className}`}
