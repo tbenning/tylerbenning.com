83afb58a278f0a2ddd6d57e6dbb4341637b525a9
Tyler Benning <tbenning27@gmail.com>
2021-12-16 20:37:09 -0800

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
 import "../styles/tailwind-custom.css"
 import "../styles/fonts.css"
+import { useEffect } from "react"
+
 import type { AppProps } from "next/app"
+import { useRouter } from "next/router"
+
+import * as ga from "../lib/ga"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
